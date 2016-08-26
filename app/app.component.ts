0949bebab0e07e3f9978fd2c6310a35ccfb6da01
RubyBe and Russ Davies <rubybe+russ@>
2016-08-25 17:36:49 -0700

    add basic skeleton, model, and app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,24 @@
 // import Component code from angulary library
 import { Component } from 'angular2/core';
+import {Meal} from './meal.model';
 
 // annotation
 @Component({
   selector: 'my-app',
   template: `
     <div class = "container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Meal Tracker!</h1>
     </div>
   `
 })
 // class declaration
 export class AppComponent {
+  public meals: Meal[];
 
+  constructor(){
+    this.meals = [
+      new Meal("Mac & Cheese", "Yummy but sinful", 310),
+      new Meal("Carrots", "What you should always eat!", 25)
+    ];
+  }
 }
